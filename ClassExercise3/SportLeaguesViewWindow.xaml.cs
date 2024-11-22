4074f629e4da9288398a5ff37ee5ef3d76043513
bradyinglisdc <brady.r.inglis@gmail.com>
2024-11-21 19:45:16 -0500

    Data sets added

---
+++ ClassExercise3/SportLeaguesViewWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Windows;
+
+
+namespace ClassExercise3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class SportLeaguesViewWindow : Window
+    {
+        public SportLeaguesViewWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
