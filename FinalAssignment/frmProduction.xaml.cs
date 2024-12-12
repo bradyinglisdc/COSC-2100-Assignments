c5be19ccf54572500414b3c7ebad8ed818086239
bradyinglisdc <brady.r.inglis@gmail.com>
2024-12-12 06:41:35 -0500

    Piano roll and timeline styled

--- FinalAssignment/frmProduction.xaml.cs
+++ FinalAssignment/frmProduction.xaml.cs
@@ -9,16 +9,10 @@
 
 #region Namespaces Used
 
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using FinalAssignment.Models;
 
 #endregion
@@ -34,11 +28,27 @@ namespace FinalAssignment
     {
         #region Styling Constants
 
+        // Piano
         private const int TotalPianoKeys = 132;
         private const int PianoKeyHeight = 20;
         private const int BlackKeyWidth = 80;
-        private static SolidColorBrush WhiteKeyBorderBrush = new SolidColorBrush(Color.FromRgb(187, 187, 187));
+        private static SolidColorBrush PianoKeyBorderBrush = new SolidColorBrush(Color.FromRgb(187, 187, 187));
+        private static Thickness PianoKeyThickness = new Thickness(1);
 
+        // Timeline
+        private static Thickness TimelineGridThickness = new Thickness(1);
+        private static int TimelineGridWidth = 30; 
+        private static SolidColorBrush TimelineGridBorderBrush = new SolidColorBrush(Color.FromArgb(128, 187, 187, 187));
+
+
+        #endregion
+
+        #region Instance Properties
+
+        /// <summary>
+        /// Gets and sets the project which the user is producing.
+        /// </summary>
+        private Project BoundProject { get; set; }
 
         #endregion
 
@@ -46,9 +56,11 @@ namespace FinalAssignment
 
         public frmProduction()
         {
+            BoundProject = new Project() { TimelineLength = 10000 };
             InitializeComponent();
             Note.FillParents();
             CreateTimeline();
+       
         }
 
         #endregion
@@ -85,6 +97,16 @@ namespace FinalAssignment
             WindowState = WindowState.Minimized;
         }
 
+        /// <summary>
+        /// Matches piano roll vertical offset with timeline area on user scroll.
+        /// </summary>
+        /// <param name="sender">svrTimelineArea.</param>
+        /// <param name="e">Event Args.</param>
+        private void svrTimelineArea_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            svrPianoRoll.ScrollToVerticalOffset(svrTimelineArea.VerticalOffset);
+        }
+
         #endregion
 
         #region Setup
@@ -117,22 +139,80 @@ namespace FinalAssignment
             {
                 foreach (char currentKey in pianoPattern)
                 {
-                    if (currentKey == 'W')
-                    {
-                        Border key = new Border()
-                        {
-                            Height = PianoKeyHeight,
-                            Background = Brushes.White,
-                            BorderBrush = WhiteKeyBorderBrush,
-                            BorderThickness = new
-                        };
-                    }
+                    CreateKey(currentKey);
+                    CreateTimelineRow();
                 }
             }
+
+        }
+
+        /// <summary>
+        /// Styles and adds a key to pnlPianoRoll based on the char, 'W' for white, 'B' for black.
+        /// </summary>
+        /// <param name="currentKey">The char representing the key to style.</param>
+        private void CreateKey(char currentKey)
+        {
+            // Create white key
+            Border key = new Border()
+            {
+                Height = PianoKeyHeight,
+                Background = Brushes.White,
+                BorderBrush = PianoKeyBorderBrush,
+                BorderThickness = PianoKeyThickness
+            };
+
+            // Add a black key to the white key before adding if pattern is on black
+            if (currentKey == 'B')
+            {
+                key.Child = new Border()
+                {
+                    Height = PianoKeyHeight,
+                    Width = BlackKeyWidth,
+                    Background = Brushes.Black,
+                    BorderBrush = PianoKeyBorderBrush,
+                    BorderThickness = PianoKeyThickness,
+                    HorizontalAlignment = HorizontalAlignment.Left
+                };
+            }
+
+            // Add the key
+            pnlPianoRoll.Children.Add(key);
+        }
+
+        /// <summary>
+        /// Creates a row to be added to the timeline area
+        /// </summary>
+        private void CreateTimelineRow()
+        {
+            // Timeline grid to be populated
+            Grid timelineGrid = new Grid() { Height = PianoKeyHeight };
+
+            // Add a column to the timeline row grid for each 1/4 beat in timeline, i.e 250 milliseconds
+            for (int i = 0; i < BoundProject.TimelineLength / (int)Note.NoteSize.QuarterBeat; i++)
+            {
+                // Define a new column to represent this 1/4 beat
+                timelineGrid.ColumnDefinitions.Add(new ColumnDefinition()
+                {
+                    Width = new GridLength(TimelineGridWidth)
+                });
+
+                // Add a styled border to the column
+                Border beat = new Border()
+                {
+                    BorderThickness = TimelineGridThickness,
+                    BorderBrush = TimelineGridBorderBrush
+                };
+                timelineGrid.Children.Add(beat);
+
+                // Set the beat's column and row to the last index
+                Grid.SetColumn(beat, timelineGrid.ColumnDefinitions.Count - 1);
+            }
+
+            // Add the timeline grid
+            pnlTimeline.Children.Add(timelineGrid);
         }
 
         #endregion
-
     }
 }
 
